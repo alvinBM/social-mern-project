a7d885c95e6122a8b7008fed4292c17c7fbc0940
Alvin Bauma <alvinbauma10@gmail.com>
2021-10-16 12:09:20 +0100

    complete user model

--- models/user.model.js
+++ models/user.model.js
@@ -2,14 +2,20 @@
  * USER MODEL
  */
 const mongoose = require("mongoose");
+const { isEmail } = require("validator");
 
 const userSchema = mongoose.Schema({
     name : {type : String, required : true},
     lastname : {type : String, required : true},
     sexe : {type : String, required : false},
-    phone : {type : String, required : true},
-    email : {type : String, require : false},
-    password : {type : String, require : true}
+    phone : {type : String, required : true, unique : true},
+    email : {type : String, require : false, unique : true, lowercase : true, validate : [isEmail]},
+    password : {type : String, require : true},
+    followers : {type : [String]},
+    following : {type : [String]},
+    likes : {type : [String]}
+},{
+    timestamps : true
 });
 
 module.exports = mongoose.model("User", userSchema);
